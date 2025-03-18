3222f7e764a92b6366b766c0935bf42b8718d107
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2025-03-18 11:47:29 +0200

    Initial Commit

---
+++ Zamalek FC/Zamalek_FCApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zamalek_FCApp.swift
+//  Zamalek FC
+//
+//  Created by Mohamed Alwakil on 2025-03-18.
+//
+
+import SwiftUI
+
+@main
+struct Zamalek_FCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
