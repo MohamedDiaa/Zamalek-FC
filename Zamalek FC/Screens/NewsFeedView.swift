8afa59fe14ee2662465384d70d892af37e20e31f
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2025-03-19 12:29:08 +0200

    fixing layout of feed view

--- Zamalek FC/Screens/NewsFeedView.swift
+++ Zamalek FC/Screens/NewsFeedView.swift
@@ -56,12 +56,28 @@ struct NewsFeedView: View {
     func newsFeedRow(item: RSSFeedItem) -> some View {
         VStack(alignment: .trailing) {
             Text(item.title ?? "")
+                .multilineTextAlignment(.trailing)
+                .frame(maxHeight: .infinity)
                 .padding()
+
             Text(item.description ?? "")
-                .font(.caption)
+                .font(.body)
                 .foregroundStyle(.secondary)
+                .multilineTextAlignment(.trailing)
+                .padding()
+
+
+            HStack {
+                Text(item.pubDate?.formatted() ?? "")
+                    .font(.caption2)
+                    .foregroundStyle(.secondary)
+
+                Spacer(minLength: 0)
+            }
+            .padding()
 
             Divider()
+
         }
         .onTapGesture {
             guard let link = item.link,
